d9ced1fafef485f4a33a6e51620fa57da8f926fe
enesisler1905 <58097246+enesisler1905@users.noreply.github.com>
2022-12-14 20:53:33 +0300

    Initial Commit

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var NumberTextField: UITextField!
+    @IBOutlet weak var ControlLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func Button(_ sender: Any) {
+        var flag=false
+        let values=NumberTextField.text!
+        let x : Int! = Int(values)
+        if x==2||x==3 {
+            ControlLabel.text="Prime number"
+        }
+        else{
+            for i in 2 ... x/2{
+                if(x%i==0){
+                    flag=true
+                }
+            }
+            if(flag==true){
+                ControlLabel.text="Is not prime number"
+            }
+            else{
+                ControlLabel.text="Prime number"
+            }
+        }
+    }
 }
-
